75410da2410852a126bda72757501bbf7d40ec57
Joel Rorseth <joelrorseth@gmail.com>
2021-03-15 13:22:30 -0400

    Implement Lab 6

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -12,7 +12,18 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
     MatCardModule,
     BrowserModule,
     StoreUiSharedModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (module) => module.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabled', relativeLinkResolution: 'legacy' }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent],
