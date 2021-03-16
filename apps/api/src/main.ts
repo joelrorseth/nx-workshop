126b5f0febbb37506f0f906bb6ca5896882ca901
Joel Rorseth <joelrorseth@gmail.com>
2021-03-16 15:51:50 -0400

    Add deploy config for api using Docker and Heroku

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,6 +12,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Normally you would restrict this to a few vetted origins
+  app.enableCors();
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
